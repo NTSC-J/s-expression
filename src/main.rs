a05afadf6eaa43dc48dc5a610b901a4a64254fff
Fuga Kato <tresener.k@gmail.com>
2020-01-29 10:55:00 +0900

    use std::io::Result

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::prelude::v1::*;
 use std::env;
 use std::iter::Peekable;
 use std::str::Chars;
+use std::io::{Result, Error, ErrorKind};
 
 #[derive(Debug, PartialEq)]
 enum Token {
@@ -47,24 +49,23 @@ impl<'a> Iterator for Lexer<'a> {
 enum S {
     Atom(String),
     List(Vec<S>), // nilは空のリストになる
-    Invalid(String),
 }
 impl S {
-    pub fn parse_str(data: &str) -> S {
+    pub fn parse_str(data: &str) -> Result<S> {
         S::parse(&mut Lexer::new(data).peekable())
     }
-    pub fn parse(lexer: &mut Peekable<Lexer>) -> S {
+    pub fn parse(lexer: &mut Peekable<Lexer>) -> Result<S> {
         if let Some(l) = lexer.next() {
             match l {
                 Token::LParen => S::get_list(lexer),
-                Token::Atom(x) => S::Atom(x),
-                _ => S::Invalid("unexpected token".to_string()),
+                Token::Atom(x) => Ok(S::Atom(x)),
+                _ => S::data_error("unexpected token"),
             }
         } else {
-            S::Invalid("early EOF".to_string())
+            S::data_error("early EOF")
         }
     }
-    fn get_list(lexer: &mut Peekable<Lexer>) -> S {
+    fn get_list(lexer: &mut Peekable<Lexer>) -> Result<S> {
         let mut list = Vec::<S>::new();
         match lexer.peek() {
             Some(Token::RParen) => {
@@ -72,36 +73,41 @@ impl S {
                               // nil
             }
             Some(_) => {
-                list.push(S::parse(lexer)); // car
+                list.push(S::parse(lexer)?); // car
                 match lexer.peek() {
                     Some(Token::Dot) => {
                         lexer.next(); // .
                         match S::parse(lexer) {
                             // cdr
-                            S::List(x) => list.extend(x),
-                            x => list.push(x),
+                            Ok(S::List(x)) => list.extend(x),
+                            Ok(x) => list.push(x),
+                            e => return e,
                         }
                         if lexer.next() != Some(Token::RParen) {
-                            list.push(S::Invalid("expected )".to_string()))
+                            return S::data_error("expected )")
                         }
                     }
                     Some(_) => {
                         match S::get_list(lexer) {
-                            S::List(cdr) => list.extend(cdr),
-                            x => list.push(x), // Invalid
+                            Ok(S::List(cdr)) => list.extend(cdr),
+                            Ok(x) => list.push(x), // Invalid
+                            e => return e,
                         }
                     }
-                    None => return S::Invalid("early EOF in list".to_string()),
+                    None => return S::data_error("early EOF in list"),
                 }
             }
-            None => return S::Invalid("early EOF in list".to_string()),
+            None => return S::data_error("early EOF in list"),
         }
-        S::List(list)
+        Ok(S::List(list))
+    }
+    fn data_error(s: &str) -> Result<S> {
+        Err(Error::new(ErrorKind::InvalidData, s))
     }
 }
 
 fn main() {
     let s = env::args().skip(1).collect::<Vec<String>>().join(" ");
     dbg!(Lexer::new(&s).collect::<Vec<Token>>());
-    dbg!(S::parse_str(&s));
+    dbg!(S::parse_str(&s).unwrap());
 }
